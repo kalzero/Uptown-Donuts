74d47ca8fdb7af50a57cdb0052f147733bdc52ec
kalzero <calvinpang81@gmail.com>
2018-11-25 23:57:02 -0600

    add Keyframe animations, rising donut sun, and brightening the sky

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CleanWebpackPlnpmugin = require("clean-webpack-plugin");
 const extractPlugin = new ExtractTextPlugin({
     filename: "css/main.css"   
 });
